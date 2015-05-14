ef09cacaffd2e07ce8072078c42ced8508bf83a1
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-14 23:12:00 +0300

    Add Spring Boot basic application

---
+++ inventory/src/main/java/name/webdizz/jeeconf/fault/tolerance/inventory/Application.java
@@ -0,0 +1,11 @@
+package name.webdizz.jeeconf.fault.tolerance.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
