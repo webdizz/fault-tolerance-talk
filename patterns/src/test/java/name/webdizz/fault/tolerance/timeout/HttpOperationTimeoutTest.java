4091d84864b61b6f68b2b7654b29572a4c9abdc7
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-15 01:44:18 +0300

    Switch to expect SocketTimeOutException

--- patterns/src/test/java/name/webdizz/fault/tolerance/timeout/HttpOperationTimeoutTest.java
+++ patterns/src/test/java/name/webdizz/fault/tolerance/timeout/HttpOperationTimeoutTest.java
@@ -1,8 +1,9 @@
 package name.webdizz.fault.tolerance.timeout;
 
+import java.net.SocketTimeoutException;
+
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
@@ -11,11 +12,11 @@ public class HttpOperationTimeoutTest {
 
     public static final int MS = 50;
 
-    @Test(expected = ConnectTimeoutException.class)
+    @Test(expected = SocketTimeoutException.class)
     public void shouldMakeSureTimeOutCaughtUsingApacheHttpClient() throws Exception {
-        int timeout = 1;
+        int timeout = 1 * MS;
         RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(timeout * MS)
+                .setSocketTimeout(timeout)
                 .build();
         CloseableHttpClient client =
                 HttpClientBuilder.create().setDefaultRequestConfig(config).build();
