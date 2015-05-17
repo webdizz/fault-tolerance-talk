7338a57054a25bb5d4cb54a30279492d94402999
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-17 12:27:01 +0300

    Enable Hystrix dashboard and event stream for it

--- inventory/src/main/java/name/webdizz/fault/tolerance/inventory/Application.java
+++ inventory/src/main/java/name/webdizz/fault/tolerance/inventory/Application.java
@@ -2,8 +2,12 @@ package name.webdizz.fault.tolerance.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableHystrix
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- inventory/src/main/java/name/webdizz/fault/tolerance/inventory/endpoint/InventoryEndpoint.java
+++ inventory/src/main/java/name/webdizz/fault/tolerance/inventory/endpoint/InventoryEndpoint.java
@@ -8,12 +8,15 @@ import name.webdizz.fault.tolerance.inventory.domain.Inventory;
 import name.webdizz.fault.tolerance.inventory.domain.Product;
 import name.webdizz.fault.tolerance.inventory.domain.Store;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/inventories")
 public class InventoryEndpoint {
 
+    @HystrixCommand
     @RequestMapping(value = "/{store}/{product}", method = RequestMethod.GET)
-    public Inventory request(@PathVariable("store") String storeId, @PathVariable("product") String productId) {
+    public Inventory requestInventory(@PathVariable("store") String storeId, @PathVariable("product") String productId) {
         Store store = new Store(storeId);
         Product product = new Product(productId);
         return new Inventory(store, product, 1000);
