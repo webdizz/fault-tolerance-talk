67dd8d79bd9964d6fe4847ec706cd17634316828
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-19 01:37:01 +0300

    Decouple test execution from service availability

--- inventory-client/src/test/java/name/webdizz/fault/tolerance/inventory/client/InventoryRequesterTest.java
+++ inventory-client/src/test/java/name/webdizz/fault/tolerance/inventory/client/InventoryRequesterTest.java
@@ -3,14 +3,27 @@ package name.webdizz.fault.tolerance.inventory.client;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import name.webdizz.fault.tolerance.inventory.domain.Inventory;
 
+import com.squareup.okhttp.mockwebserver.MockResponse;
+import com.squareup.okhttp.mockwebserver.rule.MockWebServerRule;
+
 public class InventoryRequesterTest {
+
+    @Rule
+    public final MockWebServerRule server = new MockWebServerRule();
+
+    @Before
+    public void setUp() {
+        server.enqueue(new MockResponse().setBody("{\"store\":{\"identity\":\"23434\"},\"product\":{\"identity\":\"1225\"},\"amount\":1000,\"key\":\"1225@23434\"}"));
+    }
 
     @Test
     public void shouldPerformInventoryRequest() throws Exception {
-        Inventory inventory = new InventoryRequester(InventoryApiTestConstants.INVENTORY_SERVICE_URL)
+        Inventory inventory = new InventoryRequester("http://127.0.0.1:" + server.getPort())
                 .requestInventoryFor(InventoryApiTestConstants.DEFAULT_STORE, InventoryApiTestConstants.DEFAULT_PRODUCT);
         assertThat(inventory, notNullValue());
     }
