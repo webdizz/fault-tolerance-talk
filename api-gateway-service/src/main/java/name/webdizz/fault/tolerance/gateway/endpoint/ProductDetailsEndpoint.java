c9c9204381e65cecb8118e04ba45e69c20d5c3db
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-20 17:58:36 +0300

    Switch to another command to call inventory service

--- api-gateway-service/src/main/java/name/webdizz/fault/tolerance/gateway/endpoint/ProductDetailsEndpoint.java
+++ api-gateway-service/src/main/java/name/webdizz/fault/tolerance/gateway/endpoint/ProductDetailsEndpoint.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import name.webdizz.fault.tolerance.inventory.client.InventoryRequester;
-import name.webdizz.fault.tolerance.inventory.client.command.TimeOutInventoryRequestCommand;
+import name.webdizz.fault.tolerance.inventory.client.command.CircuitBreakerInventoryRequestCommand;
 import name.webdizz.fault.tolerance.inventory.domain.Inventory;
 import name.webdizz.fault.tolerance.inventory.domain.Product;
 import name.webdizz.fault.tolerance.inventory.domain.Store;
@@ -27,7 +27,7 @@ public class ProductDetailsEndpoint {
     public ProductDetails inventory(@PathVariable("store") String storeId, @PathVariable("product") String productId) {
         Store store = new Store(storeId);
         Product product = new Product(productId);
-        Inventory inventory = new TimeOutInventoryRequestCommand(inventoryRequester, store, product, 1000).execute();
+        Inventory inventory = new CircuitBreakerInventoryRequestCommand(inventoryRequester, store, product, false).execute();
         return new ProductDetails(inventory);
     }
 
